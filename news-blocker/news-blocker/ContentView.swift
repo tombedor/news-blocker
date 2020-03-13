4d825b1ad4a4129d0cd5bb2f420ae5c183649baa
Tom Bedor <tbedor@squareup.com>
2020-03-13 10:02:46 -0700

    add blocker

---
+++ news-blocker/news-blocker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  news-blocker
+//
+//  Created by Tom Bedor on 3/13/20.
+//  Copyright © 2020 Tom Bedor. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
